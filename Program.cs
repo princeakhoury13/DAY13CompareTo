05840d767ce952b8f98fcd9f8bf90fecb29b573a
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-24 12:45:33 +0530

    added: Taking three int from user to compare them

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ namespace generics
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Day 13 Practice Problems --> CompareTo() with Generics");
+            Console.WriteLine("Maximum using CompareTo() Mathod");
+            Console.WriteLine("Enter 1st Number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd Number: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            
+            int max = Maximum.MaximumNumber(a, b, c);
+
+            Console.WriteLine("The maximum of the given three numbers is : " + max);
+            
         }
-
-        
-
-        
 
     }
 
