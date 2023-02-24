31025629a3075905f2d3240130e2a4bc3f78bc9c
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-24 13:00:12 +0530

    added: New Compare method for string values using compareTo() method

--- Maximum.cs
+++ Maximum.cs
@@ -48,5 +48,25 @@ namespace Generics
             }
             return a;
         }
+
+        public static string MaximumString(string a, string b, string c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+
+            }
+            return a;
+        }
     }
 }
