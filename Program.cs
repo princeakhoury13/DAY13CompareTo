cab8f0214d017818b612c0636b59229d4283448b
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-24 12:55:22 +0530

    added: Taking three double from user to compare them

--- Program.cs
+++ Program.cs
@@ -8,14 +8,15 @@ namespace generics
         {
             Console.WriteLine("Maximum using CompareTo() Mathod");
             Console.WriteLine("Enter 1st Number: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter 2nd Number: ");
-            int b = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter 3rd Number: ");
-            int c = Convert.ToInt32(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
 
             
-            int max = Maximum.MaximumNumber(a, b, c);
+            
+            double max = Maximum.MaximumFloatNumber(a, b, c);
 
             Console.WriteLine("The maximum of the given three numbers is : " + max);
             
