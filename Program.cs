f443ea402147d4efdc344b0fe46b69275c4d99fb
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-24 12:39:24 +0530

    added: Welcome message at master branch

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Generics;
+
+namespace generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 13 Practice Problems --> CompareTo() with Generics");
+        }
+
+        
+
+        
+
+    }
+
+}
