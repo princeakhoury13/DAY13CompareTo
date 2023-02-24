3cc3f80c640fbc29b108604ed9fefc9489670818
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-24 12:46:07 +0530

    added: Compare using compareTo() method

---
+++ Maximum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Maximum
+    {
+        public static int MaximumNumber(int a, int b, int c)
+        {
+            if(a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)  
+            {
+                return a;
+            }
+            
+            if(b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+
+            if(c.CompareTo(a) > 0 && c.CompareTo(b) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+
+            }
+            return a;
+        }
+    }
+}
